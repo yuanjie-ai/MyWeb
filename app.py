ff592f508136b0249cf2fd034e948c34e688c15c
yuanjie <313303303@qq.com>
2019-05-01 15:31:32 +0800

    meeting

---
+++ app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = 'app'
+__author__ = 'JieYuan'
+__mtime__ = '2019-04-30'
+"""
+import pandas as pd
+from pytz import timezone
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+
+app = Flask(__name__)
+
+dic = {}
+delta = pd.to_timedelta('1d')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/meeting', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+
+@app.route('/meeting', methods=['POST'])
+def signin():
+    _ = request.form
+    name, project, progress = _['name'], _['project'], _['progress']
+    text = "{}@{}: \n{}".format(project, name, progress)
+
+    ################################################################
+    # 清空前一天数据，只保留当天数据
+    tz = timezone('Asia/Shanghai')
+    date = pd.datetime.now(tz).date()
+    if str(date - delta) in dic:
+        dic.pop(str(date - delta))
+
+    dic.setdefault(str(date), {})[name] = text
+    # from pprint import pprint
+    # pprint (dic)
+    ################################################################
+
+    # 重定向
+    # return redirect('meeting_result')  # redirect('路由') 路由：redirect(url_for('路由函数'))
+    return render_template('result.html', text=text)
+
+
+@app.route('/meeting_result', methods=['GET'])
+def meeting_result():
+    return jsonify(dic)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
